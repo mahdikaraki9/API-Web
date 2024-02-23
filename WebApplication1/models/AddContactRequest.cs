e111b4444ab12393dd195f593b236b4c79c3e513
mahdi.karaki9 <160502874+mahdikaraki9@users.noreply.github.com>
2024-02-23 02:29:15 -0800

    Add files via upload

---
+++ WebApplication1/models/AddContactRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.models
+{
+    public class AddContactRequest
+    {
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public long Phone { get; set; }
+
+        public required string Address { get; set; }
+    }
+}
---
+++ WebApplication1/models/Contact.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ContactsAPI.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public long Phone { get; set; }
+
+        public required string Address { get; set; }
+
+    }
+}
+
